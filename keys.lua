dc2b24c47f0133b2f6b9a5c156b66c1714c3ed60
officialislandsarchive <officialislandsarchive@gmail.com>
2025-01-08 05:52:24 -0600

    Update keys.lua

--- keys.lua
+++ keys.lua
@@ -1,8 +1,10 @@
 return {
     keys = {
-        "key123"
+        {key = "Bob", expiry = os.time() + 86400},
+        {key = "MikeyTheOwner123!@", expiry = math.huge},
+        {key = "AlbertTheOwner123!@", expiry = math.huge}
     },
     blacklist = {
-        123456789
+        [123456789] = {reason = "Attempted exploit."}
     }
 }
